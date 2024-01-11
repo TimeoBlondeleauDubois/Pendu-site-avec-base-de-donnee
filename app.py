3acd8ffb406fe81f52912c02361702d9ed62f6ba
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-01-11 10:39:37 +0100

    création base de donnée + page de création et de connexion

--- app.py
+++ app.py
@@ -1,6 +1,75 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
+from bcrypt import hashpw, gensalt
+import sqlite3
 
-app.route("/")
+app = Flask(__name__)
+
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('login.html')
+
+def connect_db():
+    db_path = 'Pendu.db'
+    print(f"Connecté à la base de données : {db_path}")
+    return sqlite3.connect(db_path)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_credentials(username, password):
+            return redirect(url_for('dashboard'))
+        else:
+            error = 'Nom d\'utilisateur ou mot de passe incorrect'
+    return render_template('login.html', error=error)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(f"Avant create_user: {username}, {password}")
+        if create_user(username, password):
+            print("Après create_user: Utilisateur créé avec succès")
+            return redirect(url_for('login'))
+        else:
+            print("Nom d'utilisateur déja prit")
+            return "Nom d'utilisateur déja prit"
+    return render_template('signup.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return "Tableau de bord - Connecté avec succès!"
+
+def check_credentials(username, password):
+    with connect_db() as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT Mot_De_Passe FROM User WHERE Nom_Utilisateur = ?", (username,))
+        stored_password = cursor.fetchone()
+
+    if stored_password is not None:
+        stored_password = stored_password[0]
+        if hashpw(password.encode('utf-8'), stored_password) == stored_password:
+            return True
+
+    return False
+
+
+def create_user(username, password):
+    try:
+        hashed_password = hashpw(password.encode('utf-8'), gensalt())
+        with connect_db() as db:
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO User (Nom_Utilisateur, Mot_De_Passe) VALUES (?, ?)", (username, hashed_password))
+        print(f"Utilisateur {username} créé avec succès")
+        return True
+    except sqlite3.Error as e:
+        print(f"Erreur lors de la création de l'utilisateur : {e}")
+        return False
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ create_table.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+connection = sqlite3.connect('Pendu.db')
+cursor = connection.cursor()
+
+# Table User
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS User (
+    US_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nom_Utilisateur VARCHAR UNIQUE,  -- Ajout d'UNIQUE pour créer un index
+    Mot_De_Passe VARCHAR
+);
+""")
+
+# Table Liste_De_Mots
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Liste_De_Mots (
+        Li_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Mot VARCHAR
+    );
+""")
+
+# Table Score
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Score (
+        SC_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        US_Id INTEGER,
+        Nb_Partie_Gagner TINYINT,
+        Nb_Partie_Perdu TINYINT,
+        FOREIGN KEY (US_Id) REFERENCES User(US_Id)
+    );
+""")
+connection.commit()
+connection.close()

a9c39a2c203686dd343c0a90385e06d7699e51f2
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-01-18 12:03:03 +0100

    début pendu

--- test/app.py
+++ test/app.py
@@ -1,32 +1,62 @@
-words = ['python', 'flask', 'hangman', 'web', 'game']
+from flask import Flask, render_template, request, session
+import random
 
-@app.route('/start_game', methods=['POST'])
-def start_game():
-    word_to_guess = random.choice(words).upper()
-    guessed_letters = set()
-    attempts_left = 6
-    game_state = '_' * len(word_to_guess)
-    return render_template('pendu.html', word_to_guess=word_to_guess, game_state=game_state, attempts_left=attempts_left, guessed_letters=guessed_letters)
+app = Flask(__name__)
+app.secret_key = 'votre_clé_secrète'
 
+def choisir_mot():
+    mots = ["python", "programmation", "informatique", "algorithmique", "developpement"]
+    return random.choice(mots)
 
-@app.route('/play', methods=['POST'])
-def play():
-    word_to_guess = request.form['word_to_guess']
-    guessed_letters = set(request.form['guessed_letters'])
-    attempts_left = int(request.form['attempts_left'])
-    guess = request.form['guess'].upper()
+def afficher_mot_cache(mot, lettres_trouvees):
+    mot_cache = ""
+    for lettre in mot:
+        if lettre in lettres_trouvees:
+            mot_cache += lettre
+        else:
+            mot_cache += "_"
+    return mot_cache
 
-    if guess not in guessed_letters:
-        guessed_letters.add(guess)
-        if guess not in word_to_guess:
-            attempts_left -= 1
+@app.route("/")
+def index():
+    session.clear()  # Réinitialiser la session
+    return render_template("game.html", resultat="")
 
-    game_state = ''.join(letter if letter in guessed_letters else '_' for letter in word_to_guess)
+@app.route("/jouer", methods=["POST"])
+def jouer():
+    if 'mot_a_deviner' not in session:
+        session['mot_a_deviner'] = choisir_mot()
 
-    if '_' not in game_state:
-        return render_template('pendu.html', win=True, word_to_guess=word_to_guess)
+    lettres_trouvees = session.get('lettres_trouvees', [])
+    tentatives_max = 6
+    tentatives_restantes = session.get('tentatives_restantes', tentatives_max)
 
-    if attempts_left == 0:
-        return render_template('pendu.html', lose=True, word_to_guess=word_to_guess)
+    lettre = request.form.get("lettre").lower()
 
-    return render_template('pendu.html', word_to_guess=word_to_guess, game_state=game_state, attempts_left=attempts_left, guessed_letters=guessed_letters)
+    if len(lettre) != 1 or not lettre.isalpha():
+        return render_template("game.html", resultat="Veuillez entrer une seule lettre.")
+
+    if lettre in lettres_trouvees:
+        return render_template("game.html", resultat="Vous avez déjà deviné cette lettre. Essayez une autre.")
+
+    if lettre in session['mot_a_deviner']:
+        lettres_trouvees.append(lettre)
+    else:
+        tentatives_restantes -= 1
+
+    session['lettres_trouvees'] = lettres_trouvees
+    session['tentatives_restantes'] = tentatives_restantes
+
+    mot_cache = afficher_mot_cache(session['mot_a_deviner'], lettres_trouvees)
+
+    if "_" not in mot_cache:
+        resultat = "Félicitations, vous avez deviné le mot!"
+    elif tentatives_restantes == 0:
+        resultat = "Désolé, vous avez épuisé toutes vos tentatives. Le mot était '{}'.".format(session['mot_a_deviner'])
+    else:
+        resultat = mot_cache
+
+    return render_template("game.html", resultat=resultat)
+
+if __name__ == "__main__":
+    app.run(debug=True)

87310bf9c98cfd6c120ad41905d17763c8685291
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-02-07 16:55:54 +0100

    fonctionel js

--- static/historique.js
+++ static/historique.js
@@ -1,4 +1,3 @@
-
 function masquerTableau(idTableau) {
     var tableau = document.getElementById(idTableau);
     tableau.style.display = 'none';
@@ -9,47 +8,30 @@ function afficherTableau(idTableau) {
     tableau.style.display = 'table';
 }
 
-document.querySelector('.table-section:nth-child(1) .title-container2').addEventListener('click', function() {
-    var tableauFacile = document.getElementById('table_facile');
-    if (tableauFacile.style.display === 'none') {
-        afficherTableau('table_facile');
+function toggleTable(idTableau, idArrow) {
+    var tableau = document.getElementById(idTableau);
+    if (tableau.style.display === 'none') {
+        afficherTableau(idTableau);
     } else {
-        masquerTableau('table_facile');
+        masquerTableau(idTableau);
     }
-});
+    
+    toggleArrow(idArrow);
+}
 
-document.querySelector('.table-section:nth-child(2) .title-container2').addEventListener('click', function() {
-    var tableauMoyen = document.getElementById('table_moyen');
-    if (tableauMoyen.style.display === 'none') {
-        afficherTableau('table_moyen');
-    } else {
-        masquerTableau('table_moyen');
-    }
-});
+function toggleArrow(idArrow) {
+    var arrow = document.getElementById(idArrow);
+    arrow.textContent = arrow.textContent.includes('⬇️') ? arrow.textContent.replace('⬇️', '⬆️') : arrow.textContent.replace('⬆️', '⬇️');
+}
 
-document.querySelector('.table-section:nth-child(3) .title-container2').addEventListener('click', function() {
-    var tableauDifficile = document.getElementById('table_difficile');
-    if (tableauDifficile.style.display === 'none') {
-        afficherTableau('table_difficile');
-    } else {
-        masquerTableau('table_difficile');
-    }
-});
-
-// Détecter le clic sur le titre "Facile" et basculer la direction de la flèche
 document.getElementById('facile-arrow').addEventListener('click', function() {
-    var arrow = document.getElementById(facile-arrow);
-    arrow.textContent = arrow.textContent === 'Facile: ⬇️' ? 'Facile:⬆️' : 'Facile:⬇️';
+    toggleTable('table_facile', 'facile-arrow');
 });
 
-// Détecter le clic sur le titre "Moyen" et basculer la direction de la flèche
 document.getElementById('moyen-arrow').addEventListener('click', function() {
-    var arrow = document.getElementById(moyen-arrow);
-    arrow.textContent = arrow.textContent === 'Moyen: ⬇️' ? 'Moyen:⬆️' : 'Moyen:⬇️';
+    toggleTable('table_moyen', 'moyen-arrow');
 });
 
-// Détecter le clic sur le titre "Difficile" et basculer la direction de la flèche
 document.getElementById('difficile-arrow').addEventListener('click', function() {
-    var arrow = document.getElementById(difficile-arrow);
-    arrow.textContent = arrow.textContent === 'Difficile: ⬇️' ? 'Difficile:⬆️' : 'Difficile:⬇️';
+    toggleTable('table_difficile', 'difficile-arrow');
 });
